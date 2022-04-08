c8694e8d86c1fa01f54aabf4a205d24b5c08e001
ganesh314159 <ganesh314159@gmail.com>
2022-04-08 13:15:12 +0530

    onc: Cleaning some stuff
    Signed-off-by : ganesh314159 <ganesh314159@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,8 +44,18 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := \
+	console=ttyMSM0,115200,n8 \
+	androidboot.console=ttyMSM0 \
+	androidboot.hardware=qcom \
+	msm_rtb.filter=0x237 \
+	ehci-hcd.park=3 \
+	lpm_levels.sleep_disabled=1 \
+	androidboot.bootdevice=7824900.sdhci \
+	earlycon=msm_serial_dm,0x78af000 \
+	firmware_class.path=/vendor/firmware_mnt/image \
+	androidboot.usbconfigfs=true \
+	androidboot.selinux=permissive
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_PAGESIZE := 2048
